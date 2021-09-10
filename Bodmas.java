1ea5c6d02ffae7530b4a3942864618366a68f32c
samir07dev <samir07ali@gmail.com>
2021-09-10 14:56:34 +0530

    add/sub/mul/div rule

---
+++ Bodmas.java
@@ -0,0 +1,9 @@
+class Bodmas {
+	public static void main(String [] args){
+		int a=80, b=5, c=50;
+		System.out.println(a+b+c);
+		System.out.println(a-b-c);
+		System.out.println(a*b*c);
+		System.out.println(a/b+c);
+	}
+}
