5794d39f4fa1bbcef6292714a1e4ff9a4d42b329
samir07dev <samir07ali@gmail.com>
2022-09-15 21:49:22 +0530

    Practise Question 3

---
+++ PQ_3.java
@@ -0,0 +1,7 @@
+class PQ_3 {
+    public static void main (String [] args){
+        int a = 50, b = 3;
+        int divide = a/b;
+        System.out.println("The Divide of a and b is : "+ divide);
+    }
+}
