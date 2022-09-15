126c9a2b7243b9093061698554dd9b39ae638558
samir07dev <samir07ali@gmail.com>
2022-09-15 21:28:01 +0530

    Practise Question 2

---
+++ PQ_2.java
@@ -0,0 +1,10 @@
+class PQ_2 {
+    public static void main (String [] args){
+
+        int a = 85, b = 90;
+        int sum;
+        sum = a+b;
+        System.out.println("The Sum of a and b is : " + sum);
+    }
+
+}
