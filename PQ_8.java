855f56e6334bef592ce1254ecc910e03415d31c6
samir07dev <samir07ali@gmail.com>
2022-09-16 21:41:51 +0530

    Practise Question 8

---
+++ PQ_8.java
@@ -0,0 +1,10 @@
+class PQ_8 {
+    public static void main (String [] args){
+        System.out.println("JJJJJJJJJJ    A     V                V        A             ");    
+        System.out.println("     J      A   A     V             V       A   A          ");    
+        System.out.println("     J     A     A      V          V       A      A        ");    
+        System.out.println("     J    AAAAAAAAA       V       V       AAAAAAAAAA        ");
+        System.out.println(" J   J   A         A        V   V        A          A       ");
+        System.out.println("  J J   A           A         V         A            A       ");
+    }
+}
