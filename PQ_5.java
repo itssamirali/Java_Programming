b4aa1296fa5b1eb271f84286ec4ca40b69b062c6
samir07dev <samir07ali@gmail.com>
2022-09-16 20:55:09 +0530

    Practise Question

---
+++ PQ_5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class PQ_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first Number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        System.out.println("The value of a and b is " + a * b);
+
+    }
+}
